2b3fdc1537997013bce3a17e6ff8f4928b0731ae
Matt Pieper <mpieper@techcolumbus.org>
2013-12-04 15:29:36 -0500

    Add contacts, yayyyyy

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,4 +9,31 @@ class ContactsController < ApplicationController ##SFDC contacts
     @contact = Contact.find(params[:id])
   end
   
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+  
+  def update
+     @contact = Contact.find(params[:id])
+     @contact.update_attributes(params[:contact])
+     redirect_to @contact
+  end
+  
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.RecordTypeId = "012i0000000Q2OQ"
+    user = User.first
+    @contact.OwnerId = user.Id
+    @contact.class.description["fields"].select{|field| field['type'] =~ /boolean/ }.each do |field|
+        if field['defaultValue'].nil? && field['nillable'] == false && field['defaultedOnCreate'] == true && field['createable'] == true
+          @contact.send("#{field["name"]}=", false)
+        end
+      end
+    redirect_to @contact if @contact.save
+  end
+  
 end
